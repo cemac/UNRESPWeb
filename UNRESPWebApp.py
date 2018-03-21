ffd5127ab4fe79fce0522b65801a0ea966957ce8
James O'Neill <earjjo@leeds.ac.uk>
2018-03-21 14:29:36 +0000

    Add first part of Gas Experiences form

--- UNRESPWebApp.py
+++ UNRESPWebApp.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect, url_for, request
+from wtforms import Form, StringField, validators
 
 app = Flask(__name__)
 app.secret_key="TemporaryKey"
@@ -7,6 +8,21 @@ app.secret_key="TemporaryKey"
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+#Gas experiences form
+class GasExperiencesForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+
+#Gas experiences
+@app.route('/Gas_Experiences',methods=['GET', 'POST'])
+def Gas_Experiences():
+    form = GasExperiencesForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        flash('You successfully submitted the form', 'success')
+        return redirect(url_for('Gas_Experiences'))
+    return render_template('Gas_Experiences.html',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
