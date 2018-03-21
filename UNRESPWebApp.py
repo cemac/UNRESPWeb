7e5db51fd20fe2072aeee37e4d17273923542359
James O'Neill <earjjo@leeds.ac.uk>
2018-03-21 12:04:34 +0000

    Use Bootstrap theme

--- UNRESPWebApp.py
+++ UNRESPWebApp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
 app.secret_key="TemporaryKey"
@@ -6,7 +6,7 @@ app.secret_key="TemporaryKey"
 #Index
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
