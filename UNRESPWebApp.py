6a43d40decc4abbc706f3044da1fdb0f9beefe79
James O'Neill <earjjo@leeds.ac.uk>
2018-03-21 15:20:09 +0000

    Add date and 'other obs' fields

--- UNRESPWebApp.py
+++ UNRESPWebApp.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
 from wtforms import Form, StringField, validators
+from wtforms.fields.html5 import DateField
+import datetime as dt
 
 app = Flask(__name__)
 app.secret_key="TemporaryKey"
@@ -12,14 +14,18 @@ def index():
 
 #Gas experiences form
 class GasExperiencesForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
+    date = DateField('Date',format='%Y-%m-%d')
+    otherObs = StringField('Could you see the vumo? What did it look like?\
+    For example colour, visibility. You can write any other observations in this box',\
+    [validators.Length(min=1, max=500)])
 
 #Gas experiences
 @app.route('/Gas_Experiences',methods=['GET', 'POST'])
 def Gas_Experiences():
     form = GasExperiencesForm(request.form)
     if request.method == 'POST' and form.validate():
-        name = form.name.data
+        date = form.date.data
+        otherObs = form.otherObs.data
         flash('You successfully submitted the form', 'success')
         return redirect(url_for('Gas_Experiences'))
     return render_template('Gas_Experiences.html',form=form)
